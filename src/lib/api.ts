68ed8d563401c881817177bbd47f43733e917022
Dyanesh D <you@example.com>
2025-08-16 00:44:12 +0530

    fixed axios error

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://tasty-eats-be.vercel.app/api",
   // http://localhost:3000/api
   // https://tasty-eats-be.vercel.app/api
 });
